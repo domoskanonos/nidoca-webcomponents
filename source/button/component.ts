80a499138add547fdae967b00bb1124f32340ff9
Domoskanonos <Tgg8855tt#>
2020-04-28 12:46:43 +0200

    Ripple Effekt korrigiert

--- source/button/component.ts
+++ source/button/component.ts
@@ -30,31 +30,35 @@ export class ButtonComponent extends LitElement {
 
    render() {
       return html`
-         <component-flex-container
-            class="BUTTON ${this.buttonType}"
-            @click="${() => {
-               this.clicked();
-            }}"
-            .containerProperties="${[ContainerProperties.CONTAINER_WIDTH_AUTO]}"
-            flexWrap="${FlexWrap.NO_WRAP}"
-            .flexJustifyContent="${FlexJustifyContent.CENTER}"
-            .alignItems="${AlignItems.CENTER}"
-         >
-            <component-visible
-               visibleType="${BasicService.getUniqueInstance().isNotBlank(this.leadingIcon)
-                  ? VisibleType.NORMAL
-                  : VisibleType.HIDE}"
+         <effect-ripple>
+            <component-flex-container
+               class="BUTTON ${this.buttonType}"
+               @click="${() => {
+                  this.clicked();
+               }}"
+               .containerProperties="${[ContainerProperties.CONTAINER_WIDTH_AUTO]}"
+               flexWrap="${FlexWrap.NO_WRAP}"
+               .flexJustifyContent="${FlexJustifyContent.CENTER}"
+               .alignItems="${AlignItems.CENTER}"
             >
-               <component-icon icon="${this.leadingIcon}"> </component-icon>
-            </component-visible>
-            <component-visible
-               visibleType="${BasicService.getUniqueInstance().isBlank(this.leadingIcon) ? VisibleType.NORMAL : VisibleType.HIDE}"
-               ><component-spacer spacerSize="${SpacerSize.MEDIUM}"></component-spacer
-            ></component-visible>
-            <component-typography text="${this.text}" typographyType="${TypographyType.BUTTON}"></component-typography>
-            <component-spacer spacerSize="${SpacerSize.MEDIUM}"></component-spacer>
-            <slot></slot>
-         </component-flex-container>
+               <component-visible
+                  visibleType="${BasicService.getUniqueInstance().isNotBlank(this.leadingIcon)
+                     ? VisibleType.NORMAL
+                     : VisibleType.HIDE}"
+               >
+                  <component-icon icon="${this.leadingIcon}"> </component-icon>
+               </component-visible>
+               <component-visible
+                  visibleType="${BasicService.getUniqueInstance().isBlank(this.leadingIcon)
+                     ? VisibleType.NORMAL
+                     : VisibleType.HIDE}"
+                  ><component-spacer spacerSize="${SpacerSize.MEDIUM}"></component-spacer
+               ></component-visible>
+               <component-typography text="${this.text}" typographyType="${TypographyType.BUTTON}"></component-typography>
+               <component-spacer spacerSize="${SpacerSize.MEDIUM}"></component-spacer>
+               <slot></slot>
+            </component-flex-container>
+         </effect-ripple>
       `;
    }
 
--- source/effect-ripple/component.ts
+++ source/effect-ripple/component.ts
@@ -1,66 +1,44 @@
-import { css, customElement, html, LitElement, property, query, unsafeCSS } from 'lit-element';
+import { css, customElement, html, LitElement, property, PropertyValues, query, unsafeCSS } from 'lit-element';
 
 const componentCSS = require('./component.css');
 
+/**
+ * check source: https://codepen.io/jakob-e/pen/XZoZWQ
+ */
 @customElement('effect-ripple')
 export class RippleComponent extends LitElement {
    static styles = css`
       ${unsafeCSS(componentCSS)}
    `;
 
-   @property()
-   rendered: boolean = true;
-
    @query('#rippleContainer')
    private rippleContainerElement: HTMLSpanElement | undefined;
 
-   @query('#rippleElement')
-   private rippleElement: HTMLSpanElement | undefined;
+   protected firstUpdated(_changedProperties: PropertyValues): void {
+      super.firstUpdated(_changedProperties);
+   }
 
    render() {
-      return this.rendered
-         ? html`
-              <span
-                 class="rippleContainer"
-                 id="rippleContainer"
-                 @click="${(event: MouseEvent) => {
-                    this.clicked(event);
-                 }}"
-              >
-                 <span
-                    id="rippleElement"
-                    class="ripple"
-                    @animationend="${() => {
-                       this.animationend();
-                    }}"
-                 ></span>
-                 <slot></slot>
-              </span>
-           `
-         : html`
-              <span><slot></slot></span>
-           `;
+      return html`
+         <div id="rippleContainer" anim="ripple" @click="${(event: MouseEvent | TouchEvent) => this.clicked(event)}">
+            <slot></slot>
+         </div>
+      `;
    }
 
-   private clicked(event: MouseEvent) {
-      console.log('start ripple effect...');
-      if (this.rippleElement != null) {
-         if (this.rippleContainerElement != null) {
-            let scrollWidthContainer: number = this.rippleContainerElement.scrollWidth;
-            let scrollHeightContainer: number = this.rippleContainerElement.scrollHeight;
-            this.rippleElement.style.width = (scrollWidthContainer * 2).toString().concat('px');
-            this.rippleElement.style.height = (scrollHeightContainer * 2).toString().concat('px');
-            this.rippleElement.style.left = (event.offsetX - scrollWidthContainer).toString().concat('px');
-            this.rippleElement.style.top = (event.clientY - scrollHeightContainer).toString().concat('px');
-            this.rippleElement.classList.add('animate');
+   private clicked(event: MouseEvent | TouchEvent | Touch) {
+      if (this.rippleContainerElement != undefined) {
+         if (event instanceof TouchEvent) {
+            event = event.touches ? event.touches[0] : event;
          }
-      }
-   }
-
-   private animationend() {
-      console.log('stop ripple effect...');
-      if (this.rippleElement != null) {
-         this.rippleElement.classList.remove('animate');
+         const r = this.rippleContainerElement.getBoundingClientRect(),
+            d = Math.sqrt(Math.pow(r.width, 2) + Math.pow(r.height, 2)) * 2;
+         this.rippleContainerElement.style.cssText = `--s: 0; --o: 1;`;
+         this.rippleContainerElement.offsetTop;
+         if (event instanceof MouseEvent || event instanceof Touch) {
+            this.rippleContainerElement.style.cssText = `--t: 1; --o: 0; --d: ${d}; --x:${event.clientX -
+               r.left}; --y:${event.clientY - r.top};`;
+         }
       }
    }
 }
