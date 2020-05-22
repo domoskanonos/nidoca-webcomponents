e438214c4be8f54c1d179913e2d7569537e018db
dominik.bruhn <dominik.bruhn@de.rhenus.com>
2020-05-22 15:14:12 +0200

    modified

--- source/visible/component.ts
+++ source/visible/component.ts
@@ -5,21 +5,28 @@ const componentCSS = require('./component.css');
 export enum VisibleType {
    HIDE = 'HIDE',
    INVISIBLE = 'INVISIBLE',
-   NORMAL = 'NORMAL'
+   NORMAL = 'NORMAL',
 }
 
 @customElement('nidoca-visible')
 export class NidocaVisible extends LitElement {
    static styles = css`
-      ${unsafeCSS(componentCSS)}
+      .HIDE,
+      ::slotted(.HIDE) {
+         display: none !important;
+         visibility: hidden !important;
+      }
+
+      .INVISIBLE,
+      ::slotted(.INVISIBLE) {
+         visibility: hidden !important;
+      }
    `;
 
    @property()
    visibleType: string = VisibleType.HIDE;
 
    render() {
-      return html`
-         <slot class="${this.visibleType}"></slot>
-      `;
+      return html` <slot class="${this.visibleType}"></slot> `;
    }
 }
