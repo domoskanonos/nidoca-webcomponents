3b968f4a9eee419f37bdc7f3421c85acb600b026
domoskanonos <tgz014vb>
2021-02-15 20:01:37 +0100

    modified

--- generator/generator-showcase.ts
+++ generator/generator-showcase.ts
@@ -1,5 +1,6 @@
 import {TypescriptParser} from "typescript-Parser";
 import fs from 'fs';
+
 const typescriptParser = new TypescriptParser();
 let sourceRoot = "./../core/source/";
 const parsedIndexFile = typescriptParser.parseFile(sourceRoot.concat('index.ts'), 'workspace root');
@@ -9,10 +10,48 @@ parsedIndexFile.then((indexFileContent: any) => {
         console.log("parse file: %s", filename);
         const parsedFile = typescriptParser.parseFile(sourceRoot.concat(filename), 'workspace root');
         parsedFile.then(value => {
-            console.log(JSON.stringify(value));
-            fs.writeFileSync("./../showcase/source/".concat(filename), JSON.stringify(value), {
-                encoding: "utf8",
+            let imps: any[] = value["imports"];
+            imps.forEach((imp: any) => {
+                let libraryName: string = imp.libraryName;
+                let specifiers: any[] = imp.specifiers;
+                specifiers.forEach((specifier: any) => {
+                    let spec: string = specifier.specifier;
+                });
             });
+            let exp: any[] = value["exports"];
+            let res: any[] = value["resources"];
+            let usage: any = value["usages"];
+            let dec: any[] = value["declarations"]; //clazzes
+            dec.forEach((clazz: any) => {
+                let clazzName: string = clazz.name;
+                let isExported: boolean = clazz.isExported;
+                let accessors: any[] = clazz.accessors;
+                let methods: any[] = clazz.methods;
+                if (methods) {
+                    methods.forEach((method: any) => {
+                        let name: string = method.name;
+                        let type: string = method.type;
+                        let isOptional: boolean = method.isOptional;
+                        let isStatic: boolean = method.isStatic;
+                        let isAbstract: boolean = method.isAbstract;
+                        let isAsync: boolean = method.isAsync;
+                    });
+                }
+                let properties: any[] = clazz.properties;
+                if (properties) {
+                    properties.forEach((propertie: any) => {
+                        let name: string = propertie.name;
+                        let type: string = propertie.type;
+                        let isOptional: boolean = propertie.isOptional;
+                        let isStatic: boolean = propertie.isStatic;
+                    });
+                }
+            });
+
+            let options = {
+                encoding: "utf8",
+            };
+            //fs.writeFileSync("./../showcase/source/".concat(filename), JSON.stringify(value), options);
         })
     });
 })
