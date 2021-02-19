34b4f362169fad40d46383f9fd1e1952614fbbdb
u150239 <dominik.bruhn@signal-iduna.de>
2021-02-19 15:29:48 +0100

    modified

--- generator/generator-showcase.ts
+++ generator/generator-showcase.ts
@@ -1,4 +1,4 @@
-import {TypescriptParser} from 'typescript-Parser';
+import { TypescriptParser } from 'typescript-Parser';
 import fs from 'fs';
 import Handlebars = require('handlebars');
 
@@ -8,11 +8,11 @@ let handlebarsHelpers = require('handlebars-helpers')({
 console.log('handlebarsHelpers registered');
 
 
-Handlebars.registerHelper('empty', function(list: any = undefined) {
+Handlebars.registerHelper('empty', function (list: any = undefined) {
   return list === undefined || list == null || list.length == 0;
 });
 
-Handlebars.registerHelper('toTag', function(value: any) {
+Handlebars.registerHelper('toTag', function (value: any) {
   let retval: string = '';
   for (let i = 0; i < value.length; i++) {
     let c: string = value.charAt(i);
@@ -53,50 +53,18 @@ parsedIndexFile.then((indexFileContent: any) => {
 
       parsedFile.then((value: any) => {
 
-        //console.log(JSON.stringify(value));
-
-        let fileContent: string = fs.readFileSync('./component.html', 'utf-8');
-        let template = Handlebars.compile(fileContent);
-        let output: string = template(value);
-
         let imps: any[] = value['imports'];
-        imps.forEach((imp: any) => {
-          let libraryName: string = imp.libraryName;
-          let specifiers: any[] = imp.specifiers;
-          specifiers.forEach((specifier: any) => {
-            let spec: string = specifier.specifier;
-          });
-        });
-        let exp: any[] = value['exports'];
-        let res: any[] = value['resources'];
-        let usage: any = value['usages'];
-        let dec: any[] = value['declarations']; //clazzes
+        let dec: any[] = value['declarations'];
         dec.forEach((clazz: any) => {
           let clazzName: string = clazz.name;
           let isExported: boolean = clazz.isExported;
           let accessors: any[] = clazz.accessors;
-          let methods: any[] = clazz.methods;
-          if (methods) {
-            methods.forEach((method: any) => {
-              let name: string = method.name;
-              let type: string = method.type;
-              let isOptional: boolean = method.isOptional;
-              let isStatic: boolean = method.isStatic;
-              let isAbstract: boolean = method.isAbstract;
-              let isAsync: boolean = method.isAsync;
-            });
-          }
-          let properties: any[] = clazz.properties;
-          if (properties) {
-            properties.forEach((propertie: any) => {
-              let name: string = propertie.name;
-              let type: string = propertie.type;
-              let isOptional: boolean = propertie.isOptional;
-              let isStatic: boolean = propertie.isStatic;
-            });
-          }
+          imps.push({ libraryName: 'nidoca', specifiers: [{ specifier: clazzName }] });
         });
 
+        let fileContent: string = fs.readFileSync('./component.html', 'utf-8');
+        let template = Handlebars.compile(fileContent);
+        let output: string = template(value);
         fs.writeFileSync('./../showcase/source/'.concat(destinationFilename), output, {
           encoding: 'utf8',
         });
