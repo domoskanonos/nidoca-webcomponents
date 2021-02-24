a07042f2cae3fee5986a4028bef2467c2107ff53
u150239 <dominik.bruhn@signal-iduna.de>
2021-02-24 15:49:14 +0100

    Anpassungen

---
+++ showcase/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+   entry: './source/index.ts',
+   module: {
+      rules: [
+         {
+            test: /\.ts$/,
+            use: { loader: 'ts-loader', options: { transpileOnly: true } }
+         },
+      ],
+   },
+   resolve: {
+      extensions: ['.ts', '.js'],
+   },
+   optimization: {
+      minimize: true,
+      minimizer: [
+         new TerserPlugin({
+            parallel: true,
+            terserOptions: {}
+         })
+      ]
+   },
+   performance: {
+      maxEntrypointSize: 700000
+   },
+   mode: 'production',
+   output: {
+      path: path.resolve(__dirname).concat('/dist'),
+      filename: 'showcase.js'
+   },
+   plugins: [new HtmlWebpackPlugin({ template: './source/index.html' })]
+}
