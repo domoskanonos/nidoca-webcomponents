6b1123ea2c39a49452b62d3474ff54b905981ee4
Domoskanonos <Tgg8855tt#>
2021-03-01 15:32:56 +0100

    modified

--- showcase/source/nidoca-form-combobox-showcase-page.ts
+++ showcase/source/nidoca-form-combobox-showcase-page.ts
@@ -1,72 +1,68 @@
-import {NidocaShowcaseTemplate} from "./nidoca-showcase-template";
-import { TemplateResult,property,customElement,html,LitElement } from 'lit-element';
-import { FlexContainerProperties,FlexItemProperties,FlexDirection,FlexWrap,FlexJustifyContent,FlexAlignItems,FlexAlignContent,NidocaFlex,AppData,NidocaAbstractApp,AccordionType,NidocaAccordion,NidocaAccordionItem,NidocaBox,WizardStep,NidocaWizard,NidocaChip,ProgressType,NidocaProgress,BorderProperties,BorderSize,ShadowType,NidocaBorder,NidocaFloatingContainer,ButtonType,NidocaButton,DividerType,NidocaDivider,GridJustifyItems,GridAlignItems,NidocaGrid,NidocaFormOutputData,FormProperties,NidocaForm,NidocaFormCombobox,TextType,NidocaFormText,DateType,NidocaFormDate,NidocaFormInputframe,FormOutputData,NidocaFormAbstractInputElement,IconShadowType,NidocaIcon,RichMediaType,RichMediaProperties,NidocaRichMedia,NidocaCaptcha,TargetType,NidocaLinkInterface,NidocaLink,NidocaList,NidocaListItem,NidocaTopAppBar,NavigationType,NavigationStyle,NidocaNavigation,NidocaSearchBar,NidocaNavigationLink,VisibleType,NidocaVisible,NidocaNavigationSection,NidocaTable,SpacerAlignment,SpacerSize,NidocaSpacer,NidocaTabs,NidocaTab,NidocaTabContent,NidocaTemplate,NidocaTopBottomTemplate,TypographyType,TypographyAlignment,NidocaTypography,NidocaDialog,NidocaShowcase } from '@domoskanonos/nidoca-core';
-
+import {FlexContainerProperties, FlexItemProperties, FormOutputData, TypographyType} from '@domoskanonos/nidoca-core';
+import {customElement, html, property, TemplateResult} from 'lit-element';
+import {NidocaShowcaseTemplate} from './nidoca-showcase-template';
 
 @customElement('nidoca-form-combobox-showcase-page')
 export class NidocaFormComboboxShowcasePage extends NidocaShowcaseTemplate {
+  @property()
+  name: string | undefined | null = '';
 
   @property()
-  name : string|undefined|null = '';
+  value: any | undefined | null = undefined;
 
   @property()
-  value : any|undefined|null = undefined;
+  label: string | undefined | null = '';
 
   @property()
-  label : string|undefined|null = '';
+  options: FormOutputData[] | undefined | null = [];
 
   @property()
-  options : FormOutputData[]|undefined|null = [];
+  required: boolean | undefined | null = false;
 
   @property()
-  required : boolean|undefined|null = false;
+  errorText: string | undefined | null = '';
 
   @property()
-  errorText : string | undefined|null = '';
+  infoText: string | undefined | null = '';
 
   @property()
-  infoText : string | undefined|null = '';
+  warningText: string | undefined | null = '';
 
   @property()
-  warningText : string | undefined|null = '';
+  size: number | undefined | null = undefined;
 
   @property()
-  size : number|undefined|null = undefined;
+  multiple: boolean | undefined | null = false;
 
   @property()
-  multiple : boolean|undefined|null = false;
-
-  @property()
-  selectElement : HTMLSelectElement | undefined|null = undefined;
+  selectElement: HTMLSelectElement | undefined | null = undefined;
 
   getMainComponent(): TemplateResult {
     return html`
       <nidoca-flex-container
-      .flexContainerProperties="${[
-      FlexContainerProperties.CONTAINER_WIDTH_50,
-      FlexContainerProperties.TABLET_MAX_WIDTH,
-      FlexContainerProperties.SMARTPHONE_MAX_WIDTH,
-      FlexContainerProperties.SMARTPHONE_HORIZONTAL_PADDING,
-      FlexContainerProperties.TABLET_HORIZONTAL_PADDING
-  ]}"
-      .flexItemProperties="${[FlexItemProperties.KEYLINE_ALIGNMENT_HORIZONTAL, FlexItemProperties.KEYLINE_SIZE_MEDIUM]}"
-      flexItemBasisValue="100%"
-  >
-      <nidoca-typography .typographyType="${TypographyType.H2}" text="<nidoca-form-combobox/>"></nidoca-typography>
-      <nidoca-typography .typographyType="${TypographyType.BODY1}">
-        <br />description<br /><br />
-      </nidoca-typography>
-      <nidoca-typography .typographyType="${TypographyType.H4}" text="example"></nidoca-typography>
-      <nidoca-tabs>
-        <nidoca-tab slot="tab" .selected="${true}" text="demo"></nidoca-tab>
-        <nidoca-tab slot="tab" text="source"></nidoca-tab>
-        <nidoca-tab-content slot="tabContent" .selected="${true}">
-        </nidoca-tab-content>
-        <nidoca-tab-content slot="tabContent">
-        </nidoca-tab-content>
-      </nidoca-tabs>
-  </nidoca-flex-container>
-  `;
+        .flexContainerProperties="${[
+          FlexContainerProperties.CONTAINER_WIDTH_50,
+          FlexContainerProperties.TABLET_MAX_WIDTH,
+          FlexContainerProperties.SMARTPHONE_MAX_WIDTH,
+          FlexContainerProperties.SMARTPHONE_HORIZONTAL_PADDING,
+          FlexContainerProperties.TABLET_HORIZONTAL_PADDING,
+        ]}"
+        .flexItemProperties="${[
+          FlexItemProperties.KEYLINE_ALIGNMENT_HORIZONTAL,
+          FlexItemProperties.KEYLINE_SIZE_MEDIUM,
+        ]}"
+        flexItemBasisValue="100%"
+      >
+        <nidoca-typography .typographyType="${TypographyType.H2}" text="<nidoca-form-combobox/>"></nidoca-typography>
+        <nidoca-typography .typographyType="${TypographyType.BODY1}"> <br />description<br /><br /> </nidoca-typography>
+        <nidoca-typography .typographyType="${TypographyType.H4}" text="example"></nidoca-typography>
+        <nidoca-tabs>
+          <nidoca-tab slot="tab" .selected="${true}" text="demo"></nidoca-tab>
+          <nidoca-tab slot="tab" text="source"></nidoca-tab>
+          <nidoca-tab-content slot="tabContent" .selected="${true}"> </nidoca-tab-content>
+          <nidoca-tab-content slot="tabContent"> </nidoca-tab-content>
+        </nidoca-tabs>
+      </nidoca-flex-container>
+    `;
   }
-
 }
--- showcase/source/nidoca-icon-showcase-page.ts
+++ showcase/source/nidoca-icon-showcase-page.ts
@@ -1,72 +1,68 @@
-import {NidocaShowcaseTemplate} from "./nidoca-showcase-template";
-import { TemplateResult,property,customElement,html,LitElement } from 'lit-element';
-import { FlexContainerProperties,FlexItemProperties,FlexDirection,FlexWrap,FlexJustifyContent,FlexAlignItems,FlexAlignContent,NidocaFlex,AppData,NidocaAbstractApp,AccordionType,NidocaAccordion,NidocaAccordionItem,NidocaBox,WizardStep,NidocaWizard,NidocaChip,ProgressType,NidocaProgress,BorderProperties,BorderSize,ShadowType,NidocaBorder,NidocaFloatingContainer,ButtonType,NidocaButton,DividerType,NidocaDivider,GridJustifyItems,GridAlignItems,NidocaGrid,NidocaFormOutputData,FormProperties,NidocaForm,NidocaFormCombobox,TextType,NidocaFormText,DateType,NidocaFormDate,NidocaFormInputframe,FormOutputData,NidocaFormAbstractInputElement,IconShadowType,NidocaIcon,RichMediaType,RichMediaProperties,NidocaRichMedia,NidocaCaptcha,TargetType,NidocaLinkInterface,NidocaLink,NidocaList,NidocaListItem,NidocaTopAppBar,NavigationType,NavigationStyle,NidocaNavigation,NidocaSearchBar,NidocaNavigationLink,VisibleType,NidocaVisible,NidocaNavigationSection,NidocaTable,SpacerAlignment,SpacerSize,NidocaSpacer,NidocaTabs,NidocaTab,NidocaTabContent,NidocaTemplate,NidocaTopBottomTemplate,TypographyType,TypographyAlignment,NidocaTypography,NidocaDialog,NidocaShowcase } from '@domoskanonos/nidoca-core';
-
+import {FlexContainerProperties, FlexItemProperties, TypographyType} from '@domoskanonos/nidoca-core';
+import {customElement, html, property, TemplateResult} from 'lit-element';
+import {NidocaShowcaseTemplate} from './nidoca-showcase-template';
 
 @customElement('nidoca-icon-showcase-page')
 export class NidocaIconShowcasePage extends NidocaShowcaseTemplate {
+  @property()
+  icon: string | null | undefined = '';
 
   @property()
-  icon : string | null | undefined = '';
+  color: string | undefined | null = '';
 
   @property()
-  color : string|undefined|null = '';
+  backgroundColor: string | undefined | null = '';
 
   @property()
-  backgroundColor : string|undefined|null = '';
+  iconShadowType: string | undefined | null = '';
 
   @property()
-  iconShadowType : string|undefined|null = '';
+  size: number | undefined | null = undefined;
 
   @property()
-  size : number|undefined|null = undefined;
+  sizeUnit: string | undefined | null = '';
 
   @property()
-  sizeUnit : string|undefined|null = '';
+  iconTitle: string | undefined | null = '';
 
   @property()
-  iconTitle : string|undefined|null = '';
+  withIconSpace: boolean | undefined | null = false;
 
   @property()
-  withIconSpace : boolean|undefined|null = false;
+  round: boolean | undefined | null = false;
 
   @property()
-  round : boolean|undefined|null = false;
+  clickable: boolean | undefined | null = false;
 
   @property()
-  clickable : boolean|undefined|null = false;
-
-  @property()
-  deactivated : boolean|undefined|null = false;
+  deactivated: boolean | undefined | null = false;
 
   getMainComponent(): TemplateResult {
     return html`
       <nidoca-flex-container
-      .flexContainerProperties="${[
-      FlexContainerProperties.CONTAINER_WIDTH_50,
-      FlexContainerProperties.TABLET_MAX_WIDTH,
-      FlexContainerProperties.SMARTPHONE_MAX_WIDTH,
-      FlexContainerProperties.SMARTPHONE_HORIZONTAL_PADDING,
-      FlexContainerProperties.TABLET_HORIZONTAL_PADDING
-  ]}"
-      .flexItemProperties="${[FlexItemProperties.KEYLINE_ALIGNMENT_HORIZONTAL, FlexItemProperties.KEYLINE_SIZE_MEDIUM]}"
-      flexItemBasisValue="100%"
-  >
-      <nidoca-typography .typographyType="${TypographyType.H2}" text="<nidoca-icon/>"></nidoca-typography>
-      <nidoca-typography .typographyType="${TypographyType.BODY1}">
-        <br />description<br /><br />
-      </nidoca-typography>
-      <nidoca-typography .typographyType="${TypographyType.H4}" text="example"></nidoca-typography>
-      <nidoca-tabs>
-        <nidoca-tab slot="tab" .selected="${true}" text="demo"></nidoca-tab>
-        <nidoca-tab slot="tab" text="source"></nidoca-tab>
-        <nidoca-tab-content slot="tabContent" .selected="${true}">
-        </nidoca-tab-content>
-        <nidoca-tab-content slot="tabContent">
-        </nidoca-tab-content>
-      </nidoca-tabs>
-  </nidoca-flex-container>
-  `;
+        .flexContainerProperties="${[
+          FlexContainerProperties.CONTAINER_WIDTH_50,
+          FlexContainerProperties.TABLET_MAX_WIDTH,
+          FlexContainerProperties.SMARTPHONE_MAX_WIDTH,
+          FlexContainerProperties.SMARTPHONE_HORIZONTAL_PADDING,
+          FlexContainerProperties.TABLET_HORIZONTAL_PADDING,
+        ]}"
+        .flexItemProperties="${[
+          FlexItemProperties.KEYLINE_ALIGNMENT_HORIZONTAL,
+          FlexItemProperties.KEYLINE_SIZE_MEDIUM,
+        ]}"
+        flexItemBasisValue="100%"
+      >
+        <nidoca-typography .typographyType="${TypographyType.H2}" text="<nidoca-icon/>"></nidoca-typography>
+        <nidoca-typography .typographyType="${TypographyType.BODY1}"> <br />description<br /><br /> </nidoca-typography>
+        <nidoca-typography .typographyType="${TypographyType.H4}" text="example"></nidoca-typography>
+        <nidoca-tabs>
+          <nidoca-tab slot="tab" .selected="${true}" text="demo"></nidoca-tab>
+          <nidoca-tab slot="tab" text="source"></nidoca-tab>
+          <nidoca-tab-content slot="tabContent" .selected="${true}"> </nidoca-tab-content>
+          <nidoca-tab-content slot="tabContent"> </nidoca-tab-content>
+        </nidoca-tabs>
+      </nidoca-flex-container>
+    `;
   }
-
 }
