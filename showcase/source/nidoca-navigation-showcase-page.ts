2ffd008e7e333cc3e13f365a68e95579bfe44c0c
domoskanonos <tgz014vb>
2021-03-02 21:31:03 +0100

    modified

--- showcase/source/nidoca-navigation-showcase-page.ts
+++ showcase/source/nidoca-navigation-showcase-page.ts
@@ -5,12 +5,6 @@ import {NidocaShowcaseTemplate} from './nidoca-showcase-template';
 @customElement('nidoca-navigation-showcase-page')
 export class NidocaNavigationShowcasePage extends NidocaShowcaseTemplate {
   @property()
-  closed: boolean | undefined | null = false;
-
-  @property()
-  navigationType: string | undefined | null = '';
-
-  @property()
   navigationStyle: NavigationStyle | undefined | null = undefined;
 
   getMainComponent(): TemplateResult {
