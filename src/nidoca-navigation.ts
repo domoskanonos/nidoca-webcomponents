ff2383e1dbff80a310597d85797ac5d050282f3e
U150239 <u1506666>
2021-07-02 15:19:20 +0200

    A

--- src/nidoca-navigation.ts
+++ src/nidoca-navigation.ts
@@ -1,41 +1,13 @@
 import {css, html, TemplateResult, LitElement} from "lit";
 import {customElement} from "lit/decorators/custom-element";
-import {property} from "lit/decorators/property";
-
-export enum NavigationStyle {
-  SURFACE = "SURFACE",
-  PRIMARY = "PRIMARY",
-  SECONDARY = "SECONDARY",
-}
 
 @customElement("nidoca-navigation")
 export class NidocaNavigation extends LitElement {
-  static styles = css`
-    .SURFACE {
-      background-color: var(--app-color-surface-background);
-      color: var(--app-color-surface);
-    }
-
-    .PRIMARY {
-      background-color: var(--mdc-theme-primary);
-      color: var(--mdc-theme-on-primary);
-    }
-
-    .SECONDARY {
-      background-color: var(--app-color-secondary-background);
-      color: var(--app-color-secondary);
-    }
-
-    .NAVIGATION {
-    }
-  `;
-
-  @property({type: String})
-  navigationStyle: NavigationStyle = NavigationStyle.PRIMARY;
+  static styles = css``;
 
   render(): TemplateResult {
     return html`
-      <div class="NAVIGATION ${this.navigationStyle}">
+      <div>
         <slot name="contentBefore"></slot>
         <slot name="links"></slot>
         <slot name="contentAfter"></slot>
