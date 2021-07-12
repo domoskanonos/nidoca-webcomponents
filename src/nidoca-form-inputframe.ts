fb83f3eccc9a58bfb7f95d6027bfd31768e822f9
Dominik Bruhn <dominikbruhn@googlemail.com>
2021-07-12 19:48:04 +0200

    joijoi

--- src/nidoca-form-inputframe.ts
+++ src/nidoca-form-inputframe.ts
@@ -3,7 +3,13 @@ import {customElement} from "lit/decorators/custom-element";
 import {property} from "lit/decorators/property";
 import {NidocaBorderProperty, BorderSize} from "./nidoca-border";
 import {VisibleType} from "./nidoca-visible";
-import {FlexAlignContent} from "./nidoca-layout-flex-container";
+import {
+  FlexAlignContent,
+  FlexAlignItems,
+  FlexDirection,
+  FlexJustifyContent,
+  FlexWrap,
+} from "./nidoca-layout-flex-container";
 import {NidocaTypographyType} from "./nidoca-typography";
 import {NidocaLayoutSpacerType, NidocaLayoutSpacerSize} from "./nidoca-layout-spacer";
 
@@ -52,43 +58,54 @@ export class NidocaFormInputframe extends LitElement {
             @mouseover="${() => this.mouseover()}"
             @mouseout="${() => this.mouseout()}"
           >
-            <nidoca-layout-spacer spacerSize="${NidocaLayoutSpacerSize.SMALL}" .spacerTypes="${[NidocaLayoutSpacerType.ALL]}">
-              <nidoca-layout-flex-container .flexAlignContent="${FlexAlignContent.CENTER}" itemStyle="flex-basis: 100%;">
-                <nidoca-typography .typographyType="${NidocaTypographyType.CAPTION}" text="${this.label}"></nidoca-typography>
+            <nidoca-layout-spacer
+              spacerSize="${NidocaLayoutSpacerSize.SMALL}"
+              .spacerTypes="${[NidocaLayoutSpacerType.ALL]}"
+            >
+              <nidoca-layout-flex-container
+                .flexAlignContent="${FlexAlignContent.CENTER}"
+                itemStyle="flex-basis: 100%;"
+              >
+                <nidoca-typography
+                  .typographyType="${NidocaTypographyType.CAPTION}"
+                  text="${this.label}"
+                ></nidoca-typography>
                 <slot></slot>
               </nidoca-layout-flex-container>
             </nidoca-layout-spacer>
           </nidoca-border>
 
-          <nidoca-layout-flex-container .flexAlignContent="${FlexAlignContent.CENTER}" itemStyle="flex-basis: 100%;"
-            ><nidoca-visible visibleType="${this.infoText ? VisibleType.NORMAL : VisibleType.HIDE}">
-              <nidoca-typography .typographyType="${NidocaTypographyType.BODY1}" text="${this.infoText}"></nidoca-typography>
-            </nidoca-visible>
+          <nidoca-visible visibleType="${this.infoText ? VisibleType.NORMAL : VisibleType.HIDE}">
+            <nidoca-typography
+              .typographyType="${NidocaTypographyType.BODY1}"
+              text="${this.infoText}"
+            ></nidoca-typography>
+          </nidoca-visible>
 
-            <nidoca-visible visibleType="${this.warningText ? VisibleType.NORMAL : VisibleType.HIDE}">
-              <nidoca-typography
-                style="color:var(--app-color-warning-background)"
-                .typographyType="${NidocaTypographyType.BODY1}"
-                text="${this.warningText}"
-              ></nidoca-typography>
-            </nidoca-visible>
+          <nidoca-visible visibleType="${this.warningText ? VisibleType.NORMAL : VisibleType.HIDE}">
+            <nidoca-typography
+              style="color:var(--app-color-warning-background)"
+              .typographyType="${NidocaTypographyType.BODY1}"
+              text="${this.warningText}"
+            ></nidoca-typography>
+          </nidoca-visible>
 
-            <nidoca-visible visibleType="${this.errorText ? VisibleType.NORMAL : VisibleType.HIDE}">
-              <nidoca-typography
-                style="color:var(--app-color-error-background)"
-                .typographyType="${NidocaTypographyType.BODY1}"
-                text="${this.errorText}"
-              ></nidoca-typography> </nidoca-visible
-          ></nidoca-layout-flex-container>
+          <nidoca-visible visibleType="${this.errorText ? VisibleType.NORMAL : VisibleType.HIDE}">
+            <nidoca-typography
+              style="color:var(--app-color-error-background)"
+              .typographyType="${NidocaTypographyType.BODY1}"
+              text="${this.errorText}"
+            ></nidoca-typography>
+          </nidoca-visible>
         `
       : html`<slot></slot>`;
   }
 
-  async mouseover() {
+  async mouseover(): Promise<void> {
     this.selected = true;
   }
 
-  async mouseout() {
+  async mouseout(): Promise<void> {
     this.selected = false;
   }
 }
