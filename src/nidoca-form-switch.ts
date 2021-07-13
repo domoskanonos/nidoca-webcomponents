91020210a4c549ada7d7763fb5b2eb057b83c0a9
Dominik Bruhn <dominikbruhn@googlemail.com>
2021-07-13 17:25:47 +0200

    Anpassungen

--- src/nidoca-form-switch.ts
+++ src/nidoca-form-switch.ts
@@ -7,7 +7,67 @@ import {VisibleType} from "./nidoca-visible";
 
 @customElement("nidoca-form-switch")
 export class NidocaFormSwitch extends NidocaFormAbstractInputElement {
-  static styles = css``;
+  static styles = css`
+    .switch {
+      position: relative;
+      display: inline-block;
+      width: 60px;
+      height: 34px;
+    }
+
+    .switch input {
+      opacity: 0;
+      width: 0;
+      height: 0;
+    }
+
+    .slider {
+      position: absolute;
+      cursor: pointer;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: #ccc;
+      -webkit-transition: 0.4s;
+      transition: 0.4s;
+    }
+
+    .slider:before {
+      position: absolute;
+      content: "";
+      height: 26px;
+      width: 26px;
+      left: 4px;
+      bottom: 4px;
+      background-color: white;
+      -webkit-transition: 0.4s;
+      transition: 0.4s;
+    }
+
+    input:checked + .slider {
+      background-color: #2196f3;
+    }
+
+    input:focus + .slider {
+      box-shadow: 0 0 1px #2196f3;
+    }
+
+    input:checked + .slider:before {
+      -webkit-transform: translateX(26px);
+      -ms-transform: translateX(26px);
+      transform: translateX(26px);
+    }
+
+    /* Rounded sliders */
+    .slider.round {
+      border-radius: 34px;
+    }
+
+    .slider.round:before {
+      border-radius: 50%;
+    }
+  `;
 
   @property({type: String})
   name: string = "";
@@ -35,25 +95,10 @@ export class NidocaFormSwitch extends NidocaFormAbstractInputElement {
           text="${this.assistiveText}"
         ></nidoca-typography>
       </nidoca-visible>
-      <nidoca-visible visibleType="${this.checked ? VisibleType.HIDE : VisibleType.NORMAL}">
-        <nidoca-icon
-          @nidoca-event-icon-clicked="${() => {
-            this.switchSelected();
-          }}"
-          icon="toggle_off"
-          .clickable="${true}"
-        ></nidoca-icon>
-      </nidoca-visible>
-      <nidoca-visible visibleType="${this.checked ? VisibleType.NORMAL : VisibleType.HIDE}">
-        <nidoca-icon
-          style="color:var(--app-color-primary-background);"
-          @nidoca-event-icon-clicked="${() => {
-            this.switchSelected();
-          }}"
-          icon="toggle_on"
-          .clickable="${true}"
-        ></nidoca-icon>
-      </nidoca-visible>
+      <label class="switch">
+        <input id="inputElement" name="${this.name}" type="checkbox" ?checked="${this.checked}" />
+        <span class="slider round"></span>
+      </label>
     `;
   }
 
