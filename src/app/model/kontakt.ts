cde9cc8c3a30889d1c7f2c77f2ce61c0bc9f0fa0
Dominik Bruhn <Dominik.Bruhn@signal-iduna.de>
2022-03-19 19:16:17 +0100

    gzuguzg

--- src/app/model/kontakt.ts
+++ src/app/model/kontakt.ts
@@ -1,6 +1,8 @@
 export class Kontakt {
-    id: number | undefined = 0;
-    name: string = "";
-    vorname: string = "";
-    adresse: string = "";
+  id: number | undefined = 0;
+  name: string = "";
+  vorname: string = "";
+  email: string = "";
+  adresse: string = "";
+  telefon: string = "";
 }
