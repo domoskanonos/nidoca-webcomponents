b966cad351d02aa10d8581476d2c06a9ae64bb46
u150239 <Dominik.Bruhn@signal-iduna.de>
2022-06-15 11:55:13 +0200

    build corrected

--- src/app/nidoca-page-projekt.ts
+++ src/app/nidoca-page-projekt.ts
@@ -1,6 +1,6 @@
 import {css, html, LitElement, TemplateResult} from "lit";
 import {customElement} from "lit/decorators.js";
-import {CRUDProperty, GenericPostgrestController} from "..";
+import {CRUDProperty, GenericPostgrestController} from "./components/nidoca-generic-crud";
 import {NidocaPostgrestClient} from "./service/nidoca-postgrest-client";
 import {Projekt} from "./model/projekt";
 
