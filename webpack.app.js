7a64e547e19b36aca76430f7062fa7288eec0a98
u150239 <Dominik.Bruhn@signal-iduna.de>
2022-06-22 09:38:19 +0200

    ionoij

--- webpack.app.js
+++ webpack.app.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: "./src/app/index.app.ts",
+  entry: "./src/app/index.ts",
   module: {
     rules: [
       {
