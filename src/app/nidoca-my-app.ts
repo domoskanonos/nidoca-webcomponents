6358ad4ba0d6d322679df811c70bcb83744edc9b
u150239 <Dominik.Bruhn@signal-iduna.de>
2022-06-22 10:43:47 +0200

    Anpassungen

--- src/app/nidoca-my-app.ts
+++ src/app/nidoca-my-app.ts
@@ -1,66 +1,28 @@
 import {html, LitElement, TemplateResult} from "lit";
 import {customElement} from "lit/decorators.js";
-import {property} from "lit/decorators.js";
-import {NidocaRouteListener, NidocaRouter} from "@domoskanonos/nidoca-router";
-import {NidocaPostgrestClient} from "./service/nidoca-postgrest-client";
-import {NidocaStore, NidocaStoreListener} from "./service/nidoca-store";
-import {AppController, ChannelsEnum} from "./service/app-controller";
-import {PageReference} from "../apps/nidoca-app";
 
 @customElement("nidoca-my-app")
-export class NidocaMyApp extends LitElement implements NidocaStoreListener, NidocaRouteListener {
-  @property({type: Array})
-  pages: PageReference[] = [];
-
-  @property({type: Boolean})
-  //loggedIn: boolean = NidocaPostgrestClient.isLoggedIn();
-  loggedIn: boolean = true;
-
-  @property({type: Object})
-  route: string = "";
-
-  constructor() {
-    super();
-    NidocaRouter.getUniqueInstance().subscribe(this);
-    this.routeChanged(NidocaRouter.getUniqueInstance().getCurrentPage());
-    NidocaStore.addListener(this);
-  }
-
-  routeChanged(relUrl: string): void {
-    this.route = relUrl;
-  }
-
-  channelUpdated(channel: string, item: any): void {
-    console.log(`receive item for channel: ${channel},item: ${item}`);
-    if (channel == ChannelsEnum.isLoggedIn) this.loggedIn = item;
-  }
-
+export class NidocaMyApp extends LitElement {
   render(): TemplateResult {
     return html`
-    
-    <nidoca-template>
-                <nidoca-text-body slot="topCenter"
-                "></nidoca-text-body>
-                <nidoca-icon
-                        style="padding-right:var(--space-2);"
-                        slot="topRight"
-                        @nidoca-event-icon-clicked="${() => {
-                            window.location.href = "https://github.com/domoskanonos/nidoca-webcomponents";
-                        }}" icon="logout"></nidoca-icon>
+      <nidoca-template>
+        <nidoca-link
+          slot="topRight"
+          style="padding-right:var(--space-2);"
+          targetType="_blank"
+          href="https://domoskanonos.github.io/nidoca-documentation/"
+          >Homepage</nidoca-link
+        >
 
-                <span slot="content">Hallo</span>
-
-                <nidoca-menu slot="left" theme="primary">
-                    ${this.root?.childs.map((page: PageReference) => html`
-                        <nidoca-menu-item text="${page.name}"
-                                          @click="${() => {
-                                              this.routeTo(page);
-                                          }}"></nidoca-menu-item>`
-                    )}
-                </nidoca-menu>
-            </nidoca-template>
-
-
+        <nidoca-link
+          slot="topRight"
+          style="padding-right:var(--space-2);"
+          targetType="_blank"
+          href="https://github.com/domoskanonos/nidoca-webcomponents"
+          >Github</nidoca-link
+        >
+        <span slot="content">Hallo</span>
+      </nidoca-template>
     `;
   }
 }
--- src/nidoca-link.ts
+++ src/nidoca-link.ts
@@ -2,40 +2,35 @@ import {css, html, TemplateResult, LitElement} from "lit";
 import {customElement} from "lit/decorators.js";
 import {property} from "lit/decorators.js";
 
-export enum NidocaTargetType {
-    BLANK = "_blank",
-    SELF = "_self",
-    PARENT = "_parent",
-    TOP = "_top",
-}
+export type NidocaTargetType = "_blank" | "_self" | "_parent" | "_top";
 
 @customElement("nidoca-link")
 export class NidocaLink extends LitElement {
-    static styles = css`
+  static styles = css`
     :host,
     ::slotted(:host) {
       display: inline-block;
-      line-height:1.5em;
-     }
+      line-height: 1.5em;
+    }
     a {
       color: inherit;
       background-color: inherit;
     }
   `;
 
-    @property({type: String})
-    text: string = "";
+  @property({type: String})
+  text: string = "";
 
-    @property({type: String})
-    href: string = "";
+  @property({type: String})
+  href: string = "";
 
-    @property({type: String})
-    targetType: string = NidocaTargetType.SELF;
+  @property({type: String})
+  targetType: string = "_self";
 
-    render(): TemplateResult {
-        return html`<a href="${this.href}" .target="${this.targetType}">
-            <nidoca-text>${this.text}</nidoca-text>
-            <slot></slot>
-        </a> `;
-    }
+  render(): TemplateResult {
+    return html`<a href="${this.href}" .target="${this.targetType}">
+      <nidoca-text>${this.text}</nidoca-text>
+      <slot></slot>
+    </a> `;
+  }
 }
