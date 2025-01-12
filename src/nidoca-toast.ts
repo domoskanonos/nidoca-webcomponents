bd8035690435db3c344874b05eaeed39f9d1dcfa
Dominik Bruhn <domoskanonos@googlemail.com>
2025-01-12 14:23:12 +0100

    rtdtrd

--- src/nidoca-toast.ts
+++ src/nidoca-toast.ts
@@ -1,28 +1,24 @@
-import { css, html, TemplateResult } from 'lit';
-
+import { css, html, TemplateResult, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
-import { NidocaHtml } from './nidoca-webcomponents';
 
 @customElement('nidoca-toast')
-export class NidocaToast extends NidocaHtml {
+export class NidocaToast extends LitElement {
   static styles = css`
     .toast {
       position: fixed;
       bottom: 20px;
       left: 50%;
       transform: translateX(-50%);
-      background-color: var(--toast-background-color, #323232);
-      color: var(--toast-text-color, #ffffff);
+      background-color: #323232;
+      color: white;
       padding: 16px;
       border-radius: 8px;
       box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      min-width: 200px;
+      min-width: 250px;
+      text-align: center;
       opacity: 0;
       visibility: hidden;
-      transition: opacity 0.5s ease, visibility 0.5s ease;
+      transition: opacity 0.3s ease, visibility 0.3s ease;
       z-index: 1000; /* Sicherstellen, dass der Toast über anderen Elementen liegt */
     }
 
@@ -33,45 +29,30 @@ export class NidocaToast extends NidocaHtml {
   `;
 
   @property({ type: String })
-  message: string = 'Toast Message';
+  message: string = 'Dies ist eine Testnachricht';
 
-  @property({ type: Number })
-  duration: number = 3000;
-
-  private timeoutId: number | null = null;
-
-  render(): TemplateResult {
-    return html`
-      <div class="toast" id="toast">
-        <span>${this.message}</span>
-        <button class="toast-button" @click="${this.closeToast}">&times;</button>
-      </div>
-    `;
-  }
-
-  showToast(): void {
-    const toast = this.shadowRoot?.getElementById('toast');
+  showToast(duration: number = 3000): void {
+    const toast = this.shadowRoot?.querySelector('.toast') as HTMLElement;
     if (toast) {
       toast.classList.add('show');
-      this.timeoutId = window.setTimeout(() => {
-        this.closeToast();
-      }, this.duration);
+      setTimeout(() => {
+        this.hideToast();
+      }, duration);
     }
   }
 
-  closeToast(): void {
-    const toast = this.shadowRoot?.getElementById('toast');
+  hideToast(): void {
+    const toast = this.shadowRoot?.querySelector('.toast') as HTMLElement;
     if (toast) {
       toast.classList.remove('show');
     }
-    if (this.timeoutId) {
-      clearTimeout(this.timeoutId);
-      this.timeoutId = null;
-    }
   }
 
-  static example(slotName: string = ''): TemplateResult {
-    return html`<nidoca-toast slot="${slotName}" message="Beispielnachricht"></nidoca-toast>`;
+  render(): TemplateResult {
+    return html`
+      <div class="toast">
+        ${this.message}
+      </div>
+    `;
   }
-
 }
