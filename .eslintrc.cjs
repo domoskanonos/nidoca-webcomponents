214e173c87675595b7b90ebb2b503380dcd1257d
Dominik Bruhn <domoskanonos@googlemail.com>
2025-11-26 10:23:31 +0100

    feat: Add `nidoca-img` web component and update build configuration to ES2022.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,7 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 2022,
     srcType: 'module',
   },
   plugins: ['@typescript-eslint', 'eslint-plugin-tsdoc'],
